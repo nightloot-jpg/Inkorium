dcaf3a4c64f732d7e2a85d9f575102e197ed6597
nightloot-jpg <nightloot@gmail.com>
2026-08-12 01:55:17 +0200

    rebuild feed from scratch: remove old feed route

--- src/routes/_protected.feed.tsx
+++
@@ -1,7 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router';
-import { FeedPage } from '../features/feed/components';
-
-export const Route = createFileRoute('/_protected/feed')({
-  ssr: 'data-only',
-  component: FeedPage,
-});
