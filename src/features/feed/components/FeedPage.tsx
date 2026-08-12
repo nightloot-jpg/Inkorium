5a759f8a6095c8fe54b9fc6db38914adf605a589
nightloot-jpg <nightloot@gmail.com>
2026-08-12 14:12:41 +0200

    rebuild: finalize feed layout

--- src/features/feed/components/FeedPage.tsx
+++ src/features/feed/components/FeedPage.tsx
@@ -1,17 +1,4 @@
-import { useFeed } from '../hooks/useFeed';
 import { FeedList } from './FeedList';
 import { PostComposer } from './PostComposer';
-
-export function FeedPage() {
-  const { createPost } = useFeed();
-
-  return (
-    <div className="flex w-full flex-col gap-4">
-      <PostComposer
-        isLoading={createPost.isPending}
-        onSubmit={(content, type, photos) => createPost.mutate({ content, type, photos })}
-      />
-      <FeedList />
-    </div>
-  );
-}
+import { useFeed } from '../hooks/useFeed';
+export function FeedPage(){const {createPost}=useFeed();return <div className="space-y-4"><PostComposer submitting={createPost.isPending} onSubmit={(content,type,photos)=>createPost.mutate({content,type,photos})}/><FeedList/></div>}
