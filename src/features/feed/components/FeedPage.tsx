a8b4121237ed588d074b4c2a31a04483469206da
nightloot-jpg <nightloot@gmail.com>
2026-08-12 02:01:55 +0200

    rebuild feed: recreate feed page

--- src/features/feed/components/FeedPage.tsx
+++ src/features/feed/components/FeedPage.tsx
@@ -1,17 +1,8 @@
+import { useFeed } from '../hooks/useFeed';
 import { FeedList } from './FeedList';
 import { PostComposer } from './PostComposer';
-import { useFeed } from '../hooks/useFeed';
 
 export function FeedPage() {
   const { createPost } = useFeed();
-
-  return (
-    <div className="feed-page">
-      <PostComposer
-        isLoading={createPost.isPending}
-        onSubmit={(content) => createPost.mutate({ content, kind: 'text' })}
-      />
-      <FeedList />
-    </div>
-  );
+  return <div className="mx-auto w-full max-w-[820px] space-y-4"><PostComposer isLoading={createPost.isPending} onSubmit={(content,type,photos)=>createPost.mutate({content,type,photos})}/><FeedList/></div>;
 }
