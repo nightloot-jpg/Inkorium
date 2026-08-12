bc9f7320de5218436b60657c216efeb05d88d597
nightloot-jpg <nightloot@gmail.com>
2026-08-12 20:19:45 +0200

    rebuild feed server function boundary

---
+++ src/features/feed/services/feed.functions.ts
@@ -0,0 +1,33 @@
+import { createServerFn } from '@tanstack/react-start';
+import { addComment, createPost, deletePost, editPost, getPosts, likePost, unlikePost } from './feed.server';
+
+export const getPostsFn = createServerFn({ method: 'GET' })
+  .validator((data: { pageParam?: number; limit?: number }) => data)
+  .handler(({ data }) => getPosts(data));
+
+export const createPostFn = createServerFn({ method: 'POST' })
+  .validator((data) => {
+    if (!(data instanceof FormData)) throw new Error('Se esperaba FormData');
+    return data;
+  })
+  .handler(({ data }) => createPost(data));
+
+export const likePostFn = createServerFn({ method: 'POST' })
+  .validator((data: { postId: string }) => data)
+  .handler(({ data }) => likePost(data.postId));
+
+export const unlikePostFn = createServerFn({ method: 'POST' })
+  .validator((data: { postId: string }) => data)
+  .handler(({ data }) => unlikePost(data.postId));
+
+export const addCommentFn = createServerFn({ method: 'POST' })
+  .validator((data: { postId: string; content: string; parentId?: string }) => data)
+  .handler(({ data }) => addComment(data));
+
+export const deletePostFn = createServerFn({ method: 'POST' })
+  .validator((data: { postId: string }) => data)
+  .handler(({ data }) => deletePost(data.postId));
+
+export const editPostFn = createServerFn({ method: 'POST' })
+  .validator((data: { postId: string; content: string }) => data)
+  .handler(({ data }) => editPost(data));
