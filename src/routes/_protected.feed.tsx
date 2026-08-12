c849499c27dbdf550eae066c995a5692ef0c7a99
nightloot-jpg <nightloot@gmail.com>
2026-08-12 16:53:17 +0200

    rebuild: recreate feed route

--- src/routes/_protected.feed.tsx
+++ src/routes/_protected.feed.tsx
@@ -1,23 +1,4 @@
 import { createFileRoute } from '@tanstack/react-router';
-import { FeedList } from '../features/feed/components/FeedList';
-import { PostComposer } from '../features/feed/components/PostComposer';
-import { useFeed } from '../features/feed/hooks/useFeed';
+import { FeedPage } from '../features/feed/components/FeedPage';
 
-export const Route = createFileRoute('/_protected/feed')({
-  ssr: 'data-only',
-  component: FeedRoute,
-});
-
-function FeedRoute() {
-  const { createPost } = useFeed();
-
-  return (
-    <div className="mx-auto flex w-full max-w-[820px] flex-col gap-4">
-      <PostComposer
-        isLoading={createPost.isPending}
-        onSubmit={(content, type, photos) => createPost.mutate({ content, type, photos })}
-      />
-      <FeedList />
-    </div>
-  );
-}
+export const Route=createFileRoute('/_protected/feed')({ssr:'data-only',component:FeedPage});
