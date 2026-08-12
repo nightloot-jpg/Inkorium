ddb7d5838a5eb5360eaa347a7bd1ff1b62658ba3
nightloot-jpg <nightloot@gmail.com>
2026-08-12 18:50:11 +0200

    Rebuild feed from scratch: remove old page

--- src/features/feed/components/FeedPage.tsx
+++
@@ -1,8 +0,0 @@
-import { useFeed } from '../hooks/useFeed';
-import { FeedList } from './FeedList';
-import { PostComposer } from './PostComposer';
-
-export function FeedPage(){
- const {createPost}=useFeed();
- return <div className="ink-feed"><PostComposer isLoading={createPost.isPending} onSubmit={(content,type,photos)=>createPost.mutate({content,type,photos})}/><FeedList/></div>;
-}
