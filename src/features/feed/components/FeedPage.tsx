1a30def14c508cac27f88a68beee7071e491d29c
nightloot-jpg <nightloot@gmail.com>
2026-08-12 16:42:15 +0200

    rebuild feed: wire clean implementation

--- src/features/feed/components/FeedPage.tsx
+++ src/features/feed/components/FeedPage.tsx
@@ -1,17 +1,8 @@
-import { useFeed } from '../hooks/useFeed';
+import { useFeedV2 } from '../hooks/useFeedV2';
 import { FeedComposer } from './FeedComposer';
-import { FeedList } from './FeedList';
+import { FeedListV2 } from './FeedListV2';
 
 export function FeedPage() {
-  const { createPost } = useFeed();
-
-  return (
-    <div className="feed-page">
-      <FeedComposer
-        loading={createPost.isPending}
-        onSubmit={(content, type, photos) => createPost.mutate({ content, type, photos })}
-      />
-      <FeedList />
-    </div>
-  );
+  const { createPost } = useFeedV2();
+  return <div className="feed-page"><FeedComposer loading={createPost.isPending} onSubmit={(content, type, photos) => createPost.mutate({ content, type, photos })} /><FeedListV2 /></div>;
 }
