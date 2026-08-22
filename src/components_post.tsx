08c2c993e45d95bd890655538ae569d0573d5264
nightloot-jpg <nightloot@gmail.com>
2026-08-22 17:38:51 +0200

    chore: remove orphaned post component

--- src/components_post.tsx
+++
@@ -1,6 +0,0 @@
-import React, { useState, useEffect } from 'react';
-import { supabase } from './lib/supabase';
-import { Heart, MessageCircle, Share2, Send, Copy, Globe, MoreHorizontal } from 'lucide-react';
-import { formatPostTime } from './utils';
-
-// We will inject this logic back into main.tsx later
